45ed721feeb430ea18eb4295a1600e116a868919
HugeGAMES <blazeghast001@gmail.com>
2025-10-24 22:31:18 +0800

    Update NorqueloidUniversalHub.lua

--- NorqueloidUniversalHub.lua
+++ NorqueloidUniversalHub.lua
@@ -1,4 +1,3 @@
-```lua
 -- Services
 local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
@@ -129,4 +128,3 @@ end)
 
 -- Initial status
 statusLabel.Text = "Press RightShift to toggle"
-```
